63d91c35c246a7fd2aced889d20e79173989ff70
lamine785260388 <lamine785260388@gmail.com>
2022-12-04 15:19:09 +0000

    models & association

--- reservation/notreapp/admin.py
+++ reservation/notreapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Trajet,Compagnie,Vol
 # Register your models here.
+admin.site.register(Trajet)
+admin.site.register(Compagnie)
+admin.site.register(Vol)
--- reservation/notreapp/models.py
+++ reservation/notreapp/models.py
@@ -10,12 +10,25 @@ class Compagnie(models.Model):
 
     def __str__(self):
         return self.nom
+    
+class Trajet(models.Model):
+    depart=models.CharField(max_length=255)
+    arrive=models.CharField(max_length=255)
+    class Meta:
+        ordering=['depart']
+    def __str__(self):
+        return self.depart  
+        
 
-class Article(models.Model):
-    headline = models.CharField(max_length=100)
+class Vol(models.Model):
+    prix = models.FloatField()
+    datevol=models.CharField(max_length=255)
+    heure=models.CharField(max_length=255)
     Compagnies = models.ManyToManyField(Compagnie)
+    Trajets = models.ForeignKey(Trajet, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ['attdevol']
+        ordering = ['heure']
 
-    def __str__(self
+    def __str__(self):
+        return self.prix+''+self.datevol+' '+self.Compagnies
